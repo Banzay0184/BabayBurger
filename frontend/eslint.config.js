b904cca3d75e41f0ba26aba79a152c0aac3b861a
Banzay0184 <shakhzodabidov@gmail.com>
2025-07-29 00:21:25 +0500

    0.01

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -3,10 +3,11 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
-import { globalIgnores } from 'eslint/config'
 
 export default tseslint.config([
-  globalIgnores(['dist']),
+  {
+    ignores: ['dist']
+  },
   {
     files: ['**/*.{ts,tsx}'],
     extends: [
@@ -19,5 +20,10 @@ export default tseslint.config([
       ecmaVersion: 2020,
       globals: globals.browser,
     },
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-unused-vars': 'warn',
+      'react-refresh/only-export-components': 'warn'
+    }
   },
 ])
