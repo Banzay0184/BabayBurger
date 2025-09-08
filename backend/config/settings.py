ce417f02cbd354017f12c9591a51608c7fc46ae6
Banzay0184 <shakhzodabidov@gmail.com>
2025-09-08 16:00:35 +0500

    3.9

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -277,11 +277,11 @@ else:
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME', 'babayburger'),
-        'USER': os.getenv('DB_USER', 'postgres'),
-        'PASSWORD': os.getenv('DB_PASSWORD', 'postgres'),
-        'HOST': os.getenv('DB_HOST', 'localhost'),
-        'PORT': os.getenv('DB_PORT', '5432'),
+        'NAME': config('DB_NAME', default='babayburger'),
+        'USER': config('DB_USER', default='babay'),
+        'PASSWORD': config('DB_PASSWORD', default=''),
+        'HOST': config('DB_HOST', default='localhost'),
+        'PORT': config('DB_PORT', default='5432'),
         'OPTIONS': {
             'connect_timeout': 10,
         },
