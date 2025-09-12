0c4bf84fc9a1ca55dbb5687421b8efa08a86b178
Banzay0184 <shakhzodabidov@gmail.com>
2025-09-12 17:33:12 +0500

    13.3

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -10,6 +10,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: 'index.html',
+        cashier: 'cashier.html',
         operator: 'operator.html'
       }
     }
